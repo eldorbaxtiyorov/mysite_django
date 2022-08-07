bb268548c55cb18241b892996072959d831efc0d
Eldor Baxtiyorov <91687326+eldorbaxtiyorov@users.noreply.github.com>
2022-08-07 12:36:01 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'baxtiyoroveldor2705@gmail.com'
-EMAIL_HOST_PASSWORD = 'Eldor_2705'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
